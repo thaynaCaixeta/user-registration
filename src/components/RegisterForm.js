6f1b317549b4757e319e30ed462ba3d64cd49d6a
Thayná Rocha <thayna.rocha@bancointer.com.br>
2021-01-27 18:35:46 -0300

    Add hooks to control the form

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core/"
 
+const onSubmit = function (event) {
+    event.preventDefault();
+}
+
 function RegisterForm() {
+
+    const [name, setName] = useState();
+    const [lastName, setlastName] = useState();
+    const [identification, setidentification] = useState();
+
+    const validateName = function (event) {
+        let tmpName = event.target.value;
+        if (tmpName.length >= 3) {
+            tmpName = tmpName.substr(0, 3);
+        }
+        setName(tmpName);
+    }
+
     return (
-        <form>
-            <TextField id="name" label="Name" variant="outlined" margin="normal" fullWidth />
+        <form
+            onSubmit={onSubmit}>
+            <TextField id="name" label="Name" value={name} variant="outlined" margin="normal" fullWidth
+                onChange={validateName} />
 
             <TextField id="last-name" label="Last name" variant="outlined" margin="normal" fullWidth />
 
