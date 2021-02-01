c1e9eefc94b0b271d999a813fe2b45fd65dd3ed2
Thayná Rocha <thayna.rocha@bancointer.com.br>
2021-02-01 18:36:34 -0300

    Make the form component general

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import RegisterForm from './components/RegisterForm';
 import { Container, Typography } from "@material-ui/core";
 
+function onSubmit(dados) {
+  console.log(dados);
+}
+
 function App() {
   return (
     <Container component="article" maxWidth="sm">
       <Typography component="h1" variant="h3" align="center">Sign up</Typography>
-      <RegisterForm />
+      <RegisterForm onSubmit={onSubmit} />
     </Container>
   );
 }
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,37 +1,36 @@
 import React, { useState } from "react";
-import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core/"
+import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core/";
 
-const onSubmit = function (event) {
-    event.preventDefault();
-}
+function RegisterForm({ onSubmit }) {
 
-function RegisterForm() {
-
-    const [name, setName] = useState();
-    const [lastName, setlastName] = useState();
-    const [identification, setidentification] = useState();
-
-    const validateName = function (event) {
-        let tmpName = event.target.value;
-        if (tmpName.length >= 3) {
-            tmpName = tmpName.substr(0, 3);
-        }
-        setName(tmpName);
-    }
+    const [name, setName] = useState("");
+    const [lastName, setlastName] = useState("");
+    const [identification, setidentification] = useState("");
+    const [promotions, setPromotions] = useState(true);
+    const [newsletter, setNewsletter] = useState(true);
 
     return (
         <form
-            onSubmit={onSubmit}>
+            onSubmit={(event) => {
+                event.preventDefault();
+                onSubmit({ name, lastName, identification, promotions, newsletter });
+            }}>
             <TextField id="name" label="Name" value={name} variant="outlined" margin="normal" fullWidth
-                onChange={validateName} />
+                onChange={(event) => setName(event.target.value)} />
 
-            <TextField id="last-name" label="Last name" variant="outlined" margin="normal" fullWidth />
+            <TextField id="last-name" label="Last name" variant="outlined" margin="normal" fullWidth
+                onChange={(event) => setlastName(event.target.value)} />
 
-            <TextField id="identification" label="Id" variant="outlined" margin="normal" fullWidth />
+            <TextField id="identification" label="Id" variant="outlined" margin="normal" fullWidth
+                onChange={(event) => setidentification(event.target.value)} />
 
-            <FormControlLabel label="Promotions" control={<Switch name="Promotions" color="primary" defaultChecked />} />
+            <FormControlLabel label="Promotions" control={<Switch name="Promotions" color="primary"
+                checked={promotions}
+                onChange={(event) => setPromotions(event.target.checked)} />} />
 
-            <FormControlLabel label="Newsletter" control={<Switch name="Newsletter" color="primary" defaultChecked />} />
+            <FormControlLabel label="Newsletter" control={<Switch name="Newsletter" color="primary"
+                checked={newsletter}
+                onChange={(event) => setNewsletter(event.target.checked)} />} />
 
             <Button variant="contained" color="primary" type="submit">Register</Button>
         </form>
